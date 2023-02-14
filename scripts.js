ae75ca0252cbcb18391abec907634535ac647abb
EinsteinMaker <89540768+EinsteinMaker@users.noreply.github.com>
2023-02-14 13:40:00 +0100

    new file:   art/001.png 	new file:   art/002.png 	new file:   art/003.png 	new file:   art/004.png 	new file:   art/005.png 	new file:   art/006.png 	new file:   art/007.png 	new file:   art/008.png 	new file:   art/009.png 	new file:   art/010.png 	new file:   art/011.png 	new file:   art/012.png 	new file:   art/013.png 	new file:   art/014.png 	new file:   art/015.png 	new file:   art/016.png 	new file:   art/017.png 	new file:   art/018.png 	new file:   art/019.png 	new file:   art/020.png 	new file:   art/021.png 	new file:   art/022.png 	new file:   art/023.png 	new file:   art/024.png 	new file:   art/025.png 	new file:   art/026.png 	new file:   art/027.png 	new file:   art/028.png 	modified:   index.html 	modified:   scripts.js 	modified:   styles.css

--- scripts.js
+++ scripts.js
@@ -1,10 +1,30 @@
 function openSidePage(id) {
     $("#sidebar").html($("template#" + id)[0].content.cloneNode(true))
     $("#sidebar").css("width", "100%")
+    $("#sidebar").css("opacity", "1")
     $("#main").css("transform", "scaleX(0)")
+    switch (id) {
+        case "sidebar-gimp-art":
+            let imagecount = 28
+            for (let i = 1; i < imagecount; i++) {
+                let no = i.toString().padStart(3, "0")
+                
+            }
+            break
+    
+        default:
+            break
+    }
 }
 
 function closeSidePage() {
     $("#sidebar").css("width", "0%")
+    $("#sidebar").css("opacity", "0")
     $("#main").css("transform", "scaleX(1)")
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    $("#btn-gimp-art").on("click", () => {
+        openSidePage("sidebar-gimp-art")
+    })
+})
